a4c9d82f9626735aa8e76b951553af2e846d229f
Kirill <tricksy2020@mail.ru>
2021-02-22 19:16:47 +0700

    Fixed codestyle

--- Chess2.c
+++ Chess2.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include
 #define height 8
 #define width 8
-
-// Список функций
-void king(int, int);
-void queen(int, int);
-void rook(int, int);
-void knight(int, int);
-void bishop(int, int);
-void pawn(int, int);
-void playerfirst();
-void playersecond();
-void boardchange(int, int, int, int);
-void beginingboard();
-int whitecheck(int, int);
-int blackcheck(int, int);
-
-int whitepawnstatus[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //**
-int blackpawnstatus[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+void display_start_board();
 
 // Доска
-char Board[height][width]
+char board[height][width]
         = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -32,17 +15,8 @@ char Board[height][width]
            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
 
-void boardchange(int a1, int b1, int a2, int b2)
-{
-    char temp;
-
-    temp = Board[a1][b1];
-    Board[a1][b1] = Board[a2][b2];
-    Board[a2][b2] = temp;
-}
-
 // Вывод начальной доски
-void beginingboard()
+void display_start_board()
 {
     int i, j, k = 8;
     char array[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
@@ -54,18 +28,16 @@ void beginingboard()
     for (i = 0; i < height; ++i) {
         printf("%d ", k);
         for (j = 0; j < width; ++j) {
-            printf("%c ", Board[i][j]);
+            printf("%c ", board[i][j]);
         }
         k = k - 1;
         printf("\n");
     }
 }
 
-//Проверка правильности хода
-
 int main()
 {
-    beginingboard();
+    display_start_board();
 
     return 0;
 }
